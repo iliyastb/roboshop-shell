b8866ebae86d55b06fafd0edacf9e32b4bff0a9a
iliyas <skiliyas300@gmail.com>
2023-06-05 21:46:49 +0530

    update roboshop

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,27 @@
-yum install nginx -y
-rm -rf /usr/share/nginx/html/*
-curl -L -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+source common.sh
+
+print_head "Installing nginx"
+yum install nginx -y &>>${log_file}
+status_check $?
+
+print_head "Removing Old Content"
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+status_check $?
+
+print_head "Downloading Frontend Content"
+curl -L -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+status_check $?
+
+print_head "Extracting Downloaded Frontend"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl start nginx
+unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
+
+print_head "Copying Nginx Config for RoboShop"
+cp $(code_dir)/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check $?
+
+print_head "Starting nginx"
+systemctl enable nginx &>>${log_file}
+systemctl restart nginx &>>${log_file}
+status_check $?
---
+++ redis.sh
@@ -0,0 +1,19 @@
+source common.sh
+
+print_head "Installing Redis Repo files"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log_file}
+status_check $?
+
+print_head "Enable 6.2 redis repo"
+yum module enable redis:remi-6.2 -y &>>${log_file}
+yum install redis -y &>>${log_file}
+status_check $?
+
+print_head "Update Redis Listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log_file}
+status_check $?
+
+print_head "Start Redis Service"
+systemctl enable redis &>>${log_file}
+systemctl restart redis &>>${log_file}
+status_check $?
