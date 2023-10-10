3a74b7a6e9ad209d46370be617ff71faa76d5eb1
iliyastb <skiliyas300@gmail.com>
2023-10-10 19:28:00 +0530

    Update

--- ec2-servers.sh
+++ ec2-servers.sh
@@ -7,7 +7,7 @@ create_ec2() {
   echo -e "\nset-hostname ${COMPONENT}" >>/tmp/user-data
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
-      --instance-type t3.micro \
+      --instance-type t2.micro \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
