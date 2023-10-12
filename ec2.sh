418add9d1c54c2aa77f43bda2c87281e132afaeb
iliyastb <skiliyas300@gmail.com>
2023-10-12 21:08:41 +0530

    Update

--- ec2.sh
+++ ec2.sh
@@ -1,5 +1,28 @@
 ZONE_ID="Z01272351LK3NIV2TJGOQ"
+DOMAIN="devtb.online"
 SG_NAME="allow-all"
+
+create_ec2() {
+  echo -e '#!/bin/bash' >/tmp/user-data
+  echo -e "\nset-hostname ${COMPONENT}" >>/tmp/user-data
+  PUBLIC_IP=$(aws ec2 run-instances \
+      --image-id "${AMI_ID}" \
+      --instance-type t2.micro \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" \
+      --security-group-ids "${SGID}" \
+      --user-data file:///tmp/user-data \
+      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+  sed -e "s/IPADDRESS/${PUBLIC_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN/${DOMAIN}/" route53.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
+
+  if [ $? -eq 0 ]; then
+    echo "Server Created - SUCCESS - DNS RECORD - ${COMPONENT}.${DOMAIN}"
+  else
+    echo "Server Created - FAILED - DNS RECORD - ${COMPONENT}.${DOMAIN}"
+    exit 1
+  fi
+}
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 if [ -z "${AMI_ID}" ]; then
@@ -13,28 +36,7 @@ if [ -z "${SGID}" ]; then
   exit 1
 fi
 
-create_ec2_main() {
-  echo -e '#!/bin/bash' >/tmp/user-data
-  echo -e "\nset-hostname frontend" >>/tmp/user-data
-  PUBLIC_IP=$(aws ec2 run-instances \
-      --image-id ${AMI_ID} \
-      --instance-type t2.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=frontend}, {Key=Monitor,Value=yes}]" \
-      --security-group-ids ${SGID} \
-      --user-data file:///tmp/user-data \
-      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-
-  sed -e "s/IPADDRESS/${PUBLIC_IP}/" -e "s/COMPONENT/frontend/" -e "s/DOMAIN/devtb.online/" route53-main.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
-
-  if [ $? -eq 0 ]; then
-    echo "Server Created - SUCCESS - DNS RECORD - devtb.online"
-  else
-     echo "Server Created - FAILED - DNS RECORD - devtb.online"
-     exit 1
-  fi
-}
-
 for component in frontend; do
-  create_ec2_main
+  COMPONENT="${component}-dev"
+  create_ec2
 done
