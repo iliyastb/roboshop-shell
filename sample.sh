5e13750b8acf2f37be0785611edc1cf7243eb8c1
iliyastb <skiliyas300@gmail.com>
2023-10-13 20:35:14 +0530

    Update

--- sample.sh
+++ sample.sh
@@ -1,1 +1,1 @@
-aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress]" --filters "Monitor=yes" --output text | sed -e 's/"//g'
+aws ec2 describe-instances --query "Reservations[*].Instances[*].PublicIpAddress" --filters Name=frontend, Values=running --output text | sed -e 's/"//g'
