f36aa8224dda7ddfb777f627e4341ea90feaf3f9
iliyastb <skiliyas300@gmail.com>
2023-10-16 21:56:51 +0530

    Update

--- sample.sh
+++ sample.sh
@@ -1,6 +1,6 @@
-ZONE_ID="Z01272351LK3NIV2TJGOQ"
+#ZONE_ID="Z01272351LK3NIV2TJGOQ"
 
 aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress]" --filters Name=instance-state-name,Values=running --output text | sed -e 's/"//g'
 
-sed -e "s/IPADDRESS/${PUB_IP}/" -e "s/DOMAIN/${DOMAIN}/" route53-main.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
+#sed -e "s/IPADDRESS/${PUB_IP}/" -e "s/DOMAIN/${DOMAIN}/" route53-main.json >/tmp/record.json
+#aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
