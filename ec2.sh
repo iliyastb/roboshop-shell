00fc7f2eb13d931056c862c61a62b27bfbef405d
iliyastb <skiliyas300@gmail.com>
2023-10-17 15:03:48 +0530

    Update

--- ec2.sh
+++ ec2.sh
@@ -1,22 +1,43 @@
 ZONE_ID="Z01272351LK3NIV2TJGOQ"
 DOMAIN="devtb.online"
 SG_NAME="allow-all"
-COMPONENT="frontend"
 
-create_ec2() {
+ec2() {
   echo -e '#!/bin/bash' >/tmp/user-data
   echo -e "\nset-hostname ${COMPONENT}" >>/tmp/user-data
-  aws ec2 run-instances \
-      --image-id ${AMI_ID} \
+  PRIVATE_IP=$(aws ec2 run-instances \
+      --image-id "${AMI_ID}" \
       --instance-type t2.micro \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" \
       --security-group-ids "${SGID}" \
+      --user-data file:///tmp/user-data \
+      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+  if  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN/${DOMAIN}/" route53-main.json >/tmp/record.json
+      aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
+  then
+    echo "Server Created - SUCCESS - DNS RECORD - ${COMPONENT}.${DOMAIN}"
+  else
+    echo "Server Created - FAILED - DNS RECORD - ${COMPONENT}.${DOMAIN}"
+    exit 1
+  fi
+}
+
+frontend() {
+  echo -e '#!/bin/bash' >/tmp/user-data
+  echo -e "\nset-hostname frontend" >>/tmp/user-data
+  aws ec2 run-instances \
+      --image-id "${AMI_ID}" \
+      --instance-type t2.micro \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=frontend, {Key=Monitor,Value=yes}]" \
+      --security-group-ids "${SGID}" \
       --user-data file:///tmp/user-data
 
-  sed -e "s/IPADDRESS/${PUB_IP}/" -e "s/DOMAIN/${DOMAIN}/" route53-main.json >/tmp/record1.json
-  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record1.json 2>/dev/null
+  PUBLIC_IP=$(laws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress]" --filters Name=tag:Name,Values=frontend --output text | sed -e 's/"//g')
 
-  if [ $? -eq 0 ]; then
+  if  sed -e "s/IPADDRESS/${PUBLIC_IP}/" -e "s/DOMAIN/${DOMAIN}/" route53-main.json >/tmp/record.json
+      aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
+  then
     echo "Server Created - SUCCESS - DNS RECORD - ${DOMAIN}"
   else
     echo "Server Created - FAILED - DNS RECORD - ${DOMAIN}"
@@ -24,22 +45,21 @@ create_ec2() {
   fi
 }
 
-PUB_IP=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress]" --filters Name=tag:Monitor,Values=yes --output text | sed -e 's/"//g')
-if [ -z "${PUB_IP}" ]; then
-  echo "IP not found"
-  exit 1
-fi
-
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 if [ -z "${AMI_ID}" ]; then
   echo "AMI_ID not found"
   exit 1
 fi
 
-SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=${SG_NAME}" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 if [ -z "${SGID}" ]; then
   echo "Given Security Group does not exit"
   exit 1
 fi
 
-create_ec2
+frontend
+
+#for component in catalogue cart user shipping payment mongodb mysql rabbitmq redis dispatch; do
+#  COMPONENT="${component}"
+#  ec2
+#done
