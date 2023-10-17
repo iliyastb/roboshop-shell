87e9e125ec70b6b2ce2191605da1edba6b55c898
iliyastb <skiliyas300@gmail.com>
2023-10-17 15:12:26 +0530

    Update

--- sample.sh
+++ sample.sh
@@ -1,6 +1,3 @@
-#ZONE_ID="Z01272351LK3NIV2TJGOQ"
-
-aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress]" --filters Name=tag:Name,Values=workstation --output text | sed -e 's/"//g'
-
-#sed -e "s/IPADDRESS/${PUB_IP}/" -e "s/DOMAIN/${DOMAIN}/" route53-main.json >/tmp/record.json
-#aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
+#aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress]" --filters Name=tag:Name,Values=workstation --output text | sed -e 's/"//g'
+#--tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"
+#--instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
