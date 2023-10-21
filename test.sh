23e36883b39765fc590ebcf0cd0c0321c172876c
iliyastb <skiliyas300@gmail.com>
2023-10-21 20:18:23 +0530

    Update

--- test.sh
+++ test.sh
@@ -5,7 +5,6 @@
 ZONE_ID="Z01272351LK3NIV2TJGOQ"
 DOMAIN="devtb.online"
 
-PUBLIC_IP=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress]" --filters Name=tag:Name,Values=frontend --output text | sed -e 's/"//g')
-
+PUBLIC_IP=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress]" --filters Name=tag:Name,Values=frontend --output text)
 sed -e "s/IPADDRESS/${PUBLIC_IP}/" -e "s/DOMAIN/${DOMAIN}/" route53-main.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
