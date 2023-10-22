ee1b83b43ad38718689a05c33a6a7f893636bcc7
iliyastb <skiliyas300@gmail.com>
2023-10-22 21:20:54 +0530

    Update

--- frontend.sh
+++ frontend.sh
@@ -4,11 +4,6 @@ print_head "Installing Nginx"
 yum install nginx -y &>>${log_file}
 status_check $?
 
-print_head "Starting Nginx"
-systemctl enable nginx &>>${log_file}
-systemctl start nginx &>>${log_file}
-status_check $?
-
 print_head "Removing Old Content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 status_check $?
@@ -25,4 +20,9 @@ status_check $?
 
 print_head "Copying Nginx Config for RoboShop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check $?
+
+print_head "Enable & Start the Nginx"
+systemctl enable nginx &>>${log_file}
+systemctl start nginx &>>${log_file}
 status_check $?
