7bc06d359f1581cd5c833af6f50417cfa999922e
iliyastb <skiliyas300@gmail.com>
2023-10-22 19:57:30 +0530

    Update

--- frontend.sh
+++ frontend.sh
@@ -5,23 +5,28 @@ dnf install nginx -y &>>${log_file}
 status_check $?
 
 print_head "Starting Nginx"
-systemctl enable nginx &>>${log_file} ; systemctl start nginx &>>${log_file}
+systemctl enable nginx &>>${log_file}
+systemctl start nginx &>>${log_file}
 status_check $?
 
-#print_head "Copying Nginx Config for RoboShop"
-#cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
-#status_check $?
-#
-#print_head "Removing Old Content"
-#rm -rf /usr/share/nginx/html/* &>>${log_file}
-#status_check $?
-#
-#print_head "Downloading Frontend Content"
-#curl -L -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-#status_check $?
-#
-#cd /usr/share/nginx/html/
-#
-#print_head "Extracting Downloaded Frontend"
-#unzip /tmp/frontend.zip &>>${log_file}
-#status_check $?
+print_head "Copying Nginx Config for RoboShop"
+cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check $?
+
+print_head "Removing Old Content"
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+status_check $?
+
+print_head "Downloading Frontend Content"
+curl -L -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+status_check $?
+
+cd /usr/share/nginx/html/
+
+print_head "Extracting Downloaded Frontend"
+unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
+
+print_head "Starting Nginx"
+systemctl restart nginx &>>${log_file}
+status_check $?
