9d6ac4df80a5fbe15fabc073ab0c5b2dc3609744
iliyastb <skiliyas300@gmail.com>
2023-10-22 15:38:08 +0530

    Update

--- frontend.sh
+++ frontend.sh
@@ -4,6 +4,10 @@ print_head "Installing Nginx"
 dnf install nginx -y &>>${log_file}
 status_check $?
 
+print_head "Copying Nginx Config for RoboShop"
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check $?
+
 print_head "Removing Old Content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 status_check $?
@@ -12,16 +16,12 @@ print_head "Downloading Frontend Content"
 curl -L -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 status_check $?
 
+cd /usr/share/nginx/html/
+
 print_head "Extracting Downloaded Frontend"
-cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 status_check $?
 
-print_head "Copying Nginx Config for RoboShop"
-cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
-status_check $?
-
 print_head "Starting nginx"
-systemctl enable nginx &>>${log_file}
-systemctl start nginx &>>${log_file}
+systemctl enable nginx &>>${log_file} ; systemctl start nginx &>>${log_file}
 status_check $?
