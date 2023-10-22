d9b88331f37a5d8fb081ae6fdb936fa65978063b
iliyastb <skiliyas300@gmail.com>
2023-10-22 19:52:21 +0530

    Update

--- frontend.sh
+++ frontend.sh
@@ -11,17 +11,17 @@ status_check $?
 #print_head "Copying Nginx Config for RoboShop"
 #cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 #status_check $?
-
-print_head "Removing Old Content"
-rm -rf /usr/share/nginx/html/* &>>${log_file}
-status_check $?
-
-print_head "Downloading Frontend Content"
-curl -L -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-status_check $?
-
-cd /usr/share/nginx/html/
-
-print_head "Extracting Downloaded Frontend"
-unzip /tmp/frontend.zip &>>${log_file}
-status_check $?
+#
+#print_head "Removing Old Content"
+#rm -rf /usr/share/nginx/html/* &>>${log_file}
+#status_check $?
+#
+#print_head "Downloading Frontend Content"
+#curl -L -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+#status_check $?
+#
+#cd /usr/share/nginx/html/
+#
+#print_head "Extracting Downloaded Frontend"
+#unzip /tmp/frontend.zip &>>${log_file}
+#status_check $?
